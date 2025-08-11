3f90052266e68c529e11cc27ba2d7688f33444a7
Eva Sabeeh <evas190401@gmail.com>
2025-08-12 00:11:45 +0530

    Added custom domain to config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'placehold.co',
-            port: ""
-          },
-        ],
+        // remotePatterns: [
+        //   {
+        //     protocol: 'https',
+        //     hostname: 'placehold.co',
+        //     port: ""
+        //   },
+        // ],
+        domains: ['prathmeshengineering.com'],
       },
 };
 
